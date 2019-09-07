d907bf3ccf0b0a863503b008f2e840bba7f88a57
dabill <42977352+dabartosz12@users.noreply.github.com>
2019-09-07 22:27:00 +0200

    s

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const bck = document.getElementById('backgorund');
 const anwser = document.getElementById('anwser');
 let answers = ['NIE JEST', 'TAK, JEST KURWA NAJWIEKSZY SKURWIEL',
 'SPOKO ZIOMEK Z NIEGO JEST', 'JEBAC KURWE'];
@@ -5,6 +6,8 @@ let answers = ['NIE JEST', 'TAK, JEST KURWA NAJWIEKSZY SKURWIEL',
 function newClassMate() {
     let randomNumber = Math.floor(Math.random() *(answers.length));
     anwser.innerHTML = answers[randomNumber];
+    anwser.style.display = 'block';
+    bck.style.display = 'block';
 };
 
 console.log(answers);
