ff07791cabd8d31f99c1fa91b9e5dcd4fb75477c
dabill <42977352+dabartosz12@users.noreply.github.com>
2019-09-08 22:04:59 +0200

    ssss

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
 const bck = document.getElementById('backgorund');
 const anwser = document.getElementById('anwser');
-let answers = ['NIE JEST', 'TAK, JEST KURWA NAJWIEKSZY SKURWIEL',
-'SPOKO ZIOMEK Z NIEGO JEST', 'JEBAC KURWE'];
+const body = document.getElementById('body');
+const header = document.getElementById('header');
+let answers = ['PROSTE ŻE TAK', 'TAK, JEST KURWA NAJWIEKSZY SKURWIEL',
+'A WY SOBIE SIEDZICIE JAK KOLEDZY W PARKU NA ŁAWCE'];
 
 function newClassMate() {
     let randomNumber = Math.floor(Math.random() *(answers.length));
     anwser.innerHTML = answers[randomNumber];
-    anwser.style.display = 'block';
-    bck.style.display = 'block';
+    anwser.style.visibility = 'inherit';
+    bck.style.visibility = 'inherit';
+    anwser.style.opacity = '1';
+    bck.style.opacity = '1';
 };
+
+anwser.addEventListener("click", function(){
+    if(anwser.style.visibility = 'inherit'){
+        anwser.style.visibility = 'hidden';
+        bck.style.visibility = 'hidden';
+        anwser.style.opacity = '0';
+        bck.style.opacity = '0';
+    }
+});
 
 console.log(answers);
